77e05a9da66158fe6effbdfb37d8e10b39a92197
Jesse Antoszyk <22500761+jcantosz@users.noreply.github.com>
2023-06-26 14:29:36 -0400

    Update functions for users instead of teams

--- sync-users/main.js
+++ sync-users/main.js
@@ -2,39 +2,29 @@ const fs = require("fs");
 const path = require("path");
 const { Octokit } = require("@octokit/rest");
 const { createAppAuthWithEnv } = require("./src/auth");
-const {
-  getDirectories,
-  hasUsersFile,
-  getUsersFromFile,
-  getUsersFromTeam,
-  addRepositoriesToTeam,
-  removeRepositoriesFromTeam,
-} = require("./src/utils");
+const { getDirectories, hasUsersFile, getUsersFromFile, getUsersFromTeam, addUsersToTeam, removeUsersFromTeam } = require("./src/utils");
 
 const octokit = new Octokit(createAppAuthWithEnv());
 const organizationName = process.env.INPUTS_ORGANIZATION_NAME;
 
 /**
- * Processes the repositories for the specified team.
+ * Processes the users for the specified team.
  */
 async function processTeam(teamName) {
-  // Get the repositories for the team defined in its repositories file
-  const repositoryList = getUsersFromFile(teamName);
+  // Get the users for the team defined in its users file
+  const usersList = getUsersFromFile(teamName);
 
-  // Get the repositories already in the team on GitHub
-  const teamRepositories = await getUsersFromTeam(octokit, organizationName, teamName);
+  // Get the users already in the team on GitHub
+  const teamUsers = await getUsersFromTeam(octokit, organizationName, teamName);
+  console.log(teamUsers);
 
-  // console.debug("teamRepositories", teamRepositories);
-  // console.debug("repositoryList", repositoryList);
-  //console.log(await octokit.teams.listReposInOrg({org: 'jcantosz-test-org', team_slug: 'test-team',}));
+  // Add users to the team that are defined in the file but not in the team.
+  // Update users where permissions have changed.
+  // Skip users where there are no changes to save on requests.
+  await addUsersToTeam(octokit, organizationName, teamName, teamUsers, usersList);
 
-  // Add repos to the team that are defined in the file but not in the team.
-  // Update repos where permissions have changed.
-  // Skip repos where there are no changes to save on requests.
-  await addRepositoriesToTeam(octokit, organizationName, teamName, teamRepositories, repositoryList);
-
-  // Remove repos that are part of the team on GitHub but not defined in the file
-  await removeRepositoriesFromTeam(octokit, organizationName, teamName, teamRepositories, repositoryList);
+  // Remove users that are part of the team on GitHub but not defined in the file
+  await removeUsersFromTeam(octokit, organizationName, teamName, teamUsers, usersList);
 }
 
 // Get a list of all directories in the current directory
@@ -42,9 +32,12 @@ async function main() {
   if (!organizationName) {
     throw new Error("ORGANIZATION_NAME environment variable not set");
   }
+  if (process.env.INPUTS_DRY_RUN) {
+    console.log("Dry run enabled");
+  }
   // Define the organization name
   const directories = getDirectories();
-  // Loop through each directory and process the repositories for the corresponding team
+  // Loop through each directory and process the users for the corresponding team
   for (const teamName of directories) {
     if (hasUsersFile(teamName)) {
       try {
@@ -55,4 +48,5 @@ async function main() {
     }
   }
 }
+
 main();
--- sync-users/src/utils.js
+++ sync-users/src/utils.js
@@ -12,7 +12,7 @@ function getDirectories() {
 }
 
 /**
- * Gets the path to the repositories file for the specified team.
+ * Gets the path to the users file for the specified team.
  */
 function getUsersFilePath(teamName) {
   return path.join(teamName, "users");
@@ -23,56 +23,34 @@ function hasUsersFile(teamName) {
 }
 
 /**
- * Reads the repositories file for the specified team and returns the list of repositories.
+ * Reads the users file for the specified team and returns the list of users.
  */
 function getUsersFromFile(teamName) {
   const usersFilePath = getUsersFilePath(teamName);
   return fs.readFileSync(usersFilePath, "utf-8").split("\n").filter(Boolean);
-}
-/**
- * Takes the permission level and returns the corresponding permission level for the API.
- * @param {*} permission
- * @returns
- */
-function normalizePermissions(permission) {
-  permission = permission.toLowerCase();
-  if (permission == "read") {
-    return "pull";
-  }
-  if (permission == "write") {
-    return "push";
-  }
-  return permission;
 }
 
 /**
  * Reads the team's users and returns the list of users.
  */
 async function getUsersFromTeam(octokit, organizationName, teamName) {
-  const repos = await octokit.teams.listReposInOrg({
+  console.log(`Getting users for ${teamName}`);
+  const users = await octokit.rest.teams.listMembersInOrg({
     org: organizationName,
     team_slug: teamName,
   });
-  return repos.data.map(({ full_name, role_name }) => ({
-    full_name,
-    permission: normalizePermissions(role_name),
-  }));
+  return users.data.map((user) => user.login);
 }
 
-function userInList(user, userList) {
-  return userList.contains(user);
+function userInList(username, userList) {
+  return userList.includes(username);
 }
 
-function addUser(user, teamUsers, teamName) {
-  if (userInList(user, teamUsers)) {
-    if (userPermissionsChanged(user, teamUsers)) {
-      console.log(`Updating ${user.full_name} with ${user.permission} permission in ${teamName}`);
-    } else {
-      return false;
-    }
-  } else {
-    console.log(`Adding ${user.full_name} with ${user.permission} permission to ${teamName}`);
+function addUser(username, teamUsers, teamName) {
+  if (userInList(username, teamUsers)) {
+    return false;
   }
+  console.log(`Adding ${username} as a member of ${teamName}`);
   return true;
 }
 
@@ -80,17 +58,18 @@ function addUser(user, teamUsers, teamName) {
  * Adds the specified users to the corresponding team with the specified permission level.
  */
 async function addUsersToTeam(octokit, organizationName, teamName, teamUsers, userList) {
-  for (const user of userList) {
-    if (addUsers(user, teamUsers, teamName)) {
-      // Add the user to the corresponding team with the specified permission level
-      const [owner, repoName] = user.full_name.split("/");
-      await octokit.teams.addOrUpdateRepoPermissionsInOrg({
-        org: organizationName,
-        team_slug: teamName,
-        owner,
-        repo: repoName,
-        permission: user.permission,
-      });
+  for (const username of userList) {
+    if (addUser(username, teamUsers, teamName)) {
+      if (!process.env.INPUTS_DRY_RUN) {
+        // Add the user to the corresponding team with the specified permission level
+        await octokit.teams.addOrUpdateMembershipForUserInOrg({
+          org: organizationName,
+          team_slug: teamName,
+          username: username,
+        });
+      } else {
+        console.log("Dry run: Skipping execution");
+      }
     }
   }
 }
@@ -98,22 +77,25 @@ async function addUsersToTeam(octokit, organizationName, teamName, teamUsers, us
 /**
  * Removes any users from the team that are not listed in the users file.
  */
-async function removeUsersFromTeam(octokit, organizationName, teamName, teamRepos, userList) {
+async function removeUsersFromTeam(octokit, organizationName, teamName, teamUsers, userList) {
   // Loop through each user that the team has been added to
   for (const user of teamUsers) {
-    // Check if the user appears in the repositories file
+    // Check if the user appears in the users file
     //const repoSlug = repo.full_name;
 
-    // If the user does not appear in the repositories file, remove it from the team
+    // If the user does not appear in the users file, remove it from the team
     if (!userInList(user, userList)) {
       console.log(`Removing ${user} from ${teamName}`);
-
-      await octokit.teams.removeRepoInOrg({
-        org: organizationName,
-        team_slug: teamName,
-        owner,
-        repo: repoName,
-      });
+      if (!process.env.INPUTS_DRY_RUN) {
+        await octokit.teams.removeRepoInOrg({
+          org: organizationName,
+          team_slug: teamName,
+          owner,
+          repo: repoName,
+        });
+      } else {
+        console.log("Dry run: Skipping execution");
+      }
     }
   }
 }
