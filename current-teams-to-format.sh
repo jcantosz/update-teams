9c06864eee3d7bdb043236a685a988abd9fd254e
Jesse Antoszyk <22500761+jcantosz@users.noreply.github.com>
2023-07-18 15:33:04 -0400

    script to extract current teams to format used by actions

---
+++ current-teams-to-format.sh
@@ -0,0 +1,25 @@
+# Gets all current teams in the org and puts them in the folder/file structure used by these actions
+
+ORG="<>"
+# Get teams for org
+TEAM_LIST=$(mktemp)
+gh api --paginate \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  /orgs/${ORG}/teams --jq '.[].name' > ${TEAM_LIST}
+
+for team in $(cat ${TEAM_LIST}); do
+  mkdir -p ${team}
+  # Get members
+  gh api --paginate \
+    -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    /orgs/${ORG}/teams/${team}/members --jq '.[].login' > ${team}/users
+  # Get repos
+  gh api --paginate \
+      -H "Accept: application/vnd.github+json" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      /orgs/${ORG}/teams/${team}/repos --jq '.[] |  "\(if (.permissions.admin) then "admin" elif (.permissions.maintain) then "maintain" elif (.permissions.push) then "push" elif (.permissions.triage) then "triage" else "pull" end) \(.full_name)"' > ${team}/repositories
+done
+
+rm -f ${TEAM_LIST}
